b95b8c8a61ad6406d6bd7c679820fc214734bcc4
Mikelanne <mikelanne.wood@gmail.com>
2021-04-09 19:51:49 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution here
+
+
+function findMatching(drivers, string) {
+
+    let driver = drivers.filter(d => {
+     return (d.toLowerCase() === string.toLowerCase());
+    });
+    if (driver) {
+        return driver
+    }
+}
+
+function fuzzyMatch(drivers, string) {
+    
+    let driver = drivers.filter(d => {
+        return (d.charAt(0) === string.charAt(0))
+    });
+    if (driver) {
+        return driver
+    }
+}
+
+function matchName(drivers, string) {
+    
+    let driver = drivers.filter(d => {
+        return (d.name === string)
+    });
+    if (driver) {
+        return driver
+    }
+}
